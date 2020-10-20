9fbde2eb3644972ef3000ee03bb86262dde689b4
Gabriel Davey.P <71370587+silviana217@users.noreply.github.com>
2020-10-20 19:38:00 +0700

    Create java_abstract.java

---
+++ Pertemuan 5/Praktikum PPT/java_abstract.java
@@ -0,0 +1,28 @@
+package com.pboreg;
+
+abstract class Person {
+    public String fname = "John";
+    public int age = 24;
+    public abstract void study();
+
+}
+
+class Student extends Person {
+    public int graduationYear = 2018;
+    public void study() {
+        System.out.println("Studiying all day long");
+    }
+}
+
+class MyClass {
+    public static void main(String[] args) {
+
+        Student myObj = new Student();
+
+        System.out.println("Name: " + myObj.fname);
+        System.out.println("Age: " + myObj.age);
+        System.out.println("Graduation Year " + myObj.graduationYear);
+        myObj.study();
+    }
+}
+
