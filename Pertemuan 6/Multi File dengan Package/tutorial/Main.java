8ac684ac55ea32efa202076be1b64ca3af2e6019
Gabriel Davey.P <71370587+silviana217@users.noreply.github.com>
2020-10-27 21:20:47 +0700

    Create Main.java

---
+++ Pertemuan 6/Multi File dengan Package/tutorial/Main.java
@@ -0,0 +1,17 @@
+package com.tutorial;
+
+import com.terminal.Console;
+
+ class Main {
+
+    public static void main(String[] args) {
+	Player player1 = new Player("Otong");
+	Player player2 = new Player("Ucup");
+	Player player3 = new Player("Saitama");
+	player1.show();
+	player2.show();
+	player3.show();
+
+	Console.log("hallo");
+    }
+}
