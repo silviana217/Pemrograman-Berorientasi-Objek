1b79988b4ef213d1788f290997807f8abc124bb5
Gabriel Davey.P <71370587+silviana217@users.noreply.github.com>
2020-10-27 21:35:46 +0700

    Add files via upload

---
+++ Pertemuan 6/Pengenalan Inheritance/Hero.java
@@ -0,0 +1,11 @@
+package com.tutorial;
+
+// Superclass, Parent Class
+class Hero {
+    String name;
+
+    // Method
+    void display(){
+        System.out.println("Name : " + this.name);
+    }
+}
